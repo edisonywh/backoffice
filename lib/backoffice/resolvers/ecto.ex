e21ddc1a998ca71d4c7889c20fe22e17bb36c756
Edison Yap <edisonywh@gmail.com>
2021-03-28 15:11:20 +0200

    Implement filtering engine for Ecto resolver
    With this commit, Ecto resolver can now understand complex filtering
    like this:
    
    > /admin/users?inserted_at[from]=2020-12-30&inserted_at[to]=2020-12-31
    > /admin/users?id=[gte]60
    > /admin/users?order_by=[desc]id
    
    But this is only the first step, the next step is to build a UI to go
    along with it.
    
    This commit also breaks the current search field, which previously searches across
    multiple fields as specified by users, whereas the new filtering engine expects search per
    field.
    
    Partially fixes #5

--- lib/backoffice/resolvers/ecto.ex
+++ lib/backoffice/resolvers/ecto.ex
@@ -1,4 +1,21 @@
 defmodule Backoffice.Resolvers.Ecto do
+  @moduledoc """
+  Resolver for Ecto (currently only works with Postgres)
+
+  Ships with a filtering engine that translates query params into Ecto filters.
+
+  Examples:
+
+    /admin/users?name=waldo # => substring search
+    /admin/users?id=1 # => integer equality search
+    /admin/users?verified=true # => boolean search
+    /admin/users?id=[gte]60 # => integer greater than N (opts: :gte, :gt, :lte, :lt)
+    /admin/users?order_by=[desc]id # => order_by (opts: :desc, :asc)
+    /admin/users?inserted_at[from]=2020-12-30&inserted_at[to]=2020-12-31 # => datetime search
+    /admin/users?processed_at=nil # nil
+    /admin/users?processed_at=[not]nil # not nil
+  """
+
   @behaviour Backoffice.Resolver
 
   import Ecto.Query
@@ -33,12 +50,19 @@ defmodule Backoffice.Resolvers.Ecto do
     preloads = Keyword.get(resolver_opts, :preload, [])
     page = Map.get(page_opts, "page")
 
-    # TODO: Optimize and select only shown resources (this means adding the original module here so we can access __MODULE__.fields())
-
     resource
-    |> ordering()
     |> preload([q], ^preloads)
     |> repo.paginate(%{page: page})
+  end
+
+  def search(_mod, resource, resolver_opts, page_opts) do
+    page_opts
+    |> Enum.map(&preprocess/1)
+    |> List.flatten()
+    |> Enum.reduce(resource, fn {_, field, _} = filter, acc ->
+      do_filter(acc, filter, resource.__schema__(:type, field))
+    end)
+    |> load(resolver_opts, page_opts)
   end
 
   def get(resource, resolver_opts, page_opts) do
@@ -52,30 +76,133 @@ defmodule Backoffice.Resolvers.Ecto do
     |> repo.get(id)
   end
 
-  # TODO: Let user click on fields to do ordering?
-  def ordering(resource) do
-    [field | _] = Backoffice.Resources.resolve_schema(resource).__schema__(:primary_key)
+  defp preprocess({"page", value}) do
+    {:skip, :page, value}
+  end
 
-    resource
+  defp preprocess({field, <<"[gte]", value::binary>>}) do
+    {:and, String.to_existing_atom(field), {:gte, String.to_integer(value)}}
+  end
+
+  defp preprocess({field, <<"[gt]", value::binary>>}) do
+    {:and, String.to_existing_atom(field), {:gt, String.to_integer(value)}}
+  end
+
+  defp preprocess({field, <<"[lte]", value::binary>>}) do
+    {:and, String.to_existing_atom(field), {:lte, String.to_integer(value)}}
+  end
+
+  defp preprocess({field, <<"[lt]", value::binary>>}) do
+    {:and, String.to_existing_atom(field), {:lt, String.to_integer(value)}}
+  end
+
+  defp preprocess({field, <<"[not]", value::binary>>}) do
+    {:and, String.to_existing_atom(field), {:not, maybe_nil(value)}}
+  end
+
+  defp preprocess({field, <<"[desc]", value::binary>>}) do
+    {:and, String.to_existing_atom(field), {:desc, String.to_existing_atom(value)}}
+  end
+
+  defp preprocess({field, <<"[asc]", value::binary>>}) do
+    {:and, String.to_existing_atom(field), {:asc, String.to_existing_atom(value)}}
+  end
+
+  defp preprocess({field, value}) when is_binary(value) do
+    {:and, String.to_existing_atom(field), {:contains, maybe_nil(value)}}
+  end
+
+  # If it is a query for datetime with `from` & `to` field, we convert it to
+  # a `gt` from and a `lt` to, since that's what the Ecto query looks like.
+  defp preprocess({field, value}) when is_map(value) do
+    preprocess_date(field, value)
+  end
+
+  defp preprocess_date(field, %{"from" => from, "to" => to}) do
+    [
+      {:and, String.to_existing_atom(field), {:gt, to_naive_datetime(from)}},
+      {:and, String.to_existing_atom(field), {:lt, to_naive_datetime(to)}}
+    ]
+  end
+
+  defp preprocess_date(field, %{"from" => from}) do
+    {:and, String.to_existing_atom(field), {:gt, to_naive_datetime(from)}}
+  end
+
+  defp preprocess_date(field, %{"to" => to}) do
+    {:and, String.to_existing_atom(field), {:lt, to_naive_datetime(to)}}
+  end
+
+  defp do_filter(query, {:skip, _field, _value}, _type), do: query
+
+  defp do_filter(query, {:and, field, {:lt, value}}, _type) do
+    query
+    |> where([q], field(q, ^field) < ^value)
+  end
+
+  defp do_filter(query, {:and, field, {:lte, value}}, _type) do
+    query
+    |> where([q], field(q, ^field) <= ^value)
+  end
+
+  defp do_filter(query, {:and, field, {:gt, value}}, _type) do
+    query
+    |> where([q], field(q, ^field) > ^value)
+  end
+
+  defp do_filter(query, {:and, field, {:gte, value}}, _type) do
+    query
+    |> where([q], field(q, ^field) >= ^value)
+  end
+
+  defp do_filter(query, {:and, :order_by, {:desc, field}}, _type) do
+    query
     |> order_by([q], {:desc, field(q, ^field)})
   end
 
-  def do_filter(query, "", _), do: query
-  def do_filter(query, nil, _), do: query
-  def do_filter(query, _search, []), do: query
-
-  def do_filter(query, search, [field | rest]) do
+  defp do_filter(query, {:and, :order_by, {:asc, field}}, _type) do
     query
-    |> or_where([q], ilike(field(q, ^field), ^"%#{search}%"))
-    |> do_filter(search, rest)
+    |> order_by([q], {:asc, field(q, ^field)})
   end
 
-  def search(mod, resource, resolver_opts, page_opts) do
-    fields = mod.search_fields()
-    search = Map.get(page_opts, "search")
-
-    resource
-    |> do_filter(search, fields)
-    |> load(resolver_opts, page_opts)
+  defp do_filter(query, {:and, field, {:contains, nil}}, _type) do
+    query
+    |> where([q], is_nil(field(q, ^field)))
   end
+
+  defp do_filter(query, {:and, field, {:contains, value}}, type)
+       when type in [:id, :integer, :boolean] do
+    query
+    |> where([q], field(q, ^field) == ^value)
+  end
+
+  defp do_filter(query, {:and, field, {:contains, value}}, _type) do
+    value =
+      value
+      |> String.replace("%", "\%")
+      |> String.replace("_", "\_")
+
+    value = "%#{value}%"
+
+    query
+    |> where([q], ilike(field(q, ^field), ^value))
+  end
+
+  defp do_filter(query, {:and, field, {:not, nil}}, _type) do
+    query
+    |> where([q], not is_nil(field(q, ^field)))
+  end
+
+  defp do_filter(query, {:and, field, {:not, value}}, _type) do
+    query
+    |> where([q], field(q, ^field) != ^value)
+  end
+
+  defp to_naive_datetime(string) do
+    {:ok, date} = NaiveDateTime.new(Date.from_iso8601!(string), ~T[00:00:00])
+    date
+  end
+
+  defp maybe_nil("nil"), do: nil
+  defp maybe_nil(string), do: string
 end
