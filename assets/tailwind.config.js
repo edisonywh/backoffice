5458ecac7d8eae35189112402c4009942289af04
Edison Yap <edisonywh@gmail.com>
2021-03-30 19:01:56 +0200

    Fix Tailwind static files

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   purge: [
+    '../lib/backoffice/components/*.ex',
     '../lib/backoffice/templates/**/*.eex',
     '../lib/backoffice/templates/**/*.leex',
     '../lib/backoffice/views/*.ex',
