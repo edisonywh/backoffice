3e6a8c9a73b92de515b5fc06d6cfee8c3668e734
Edison Yap <edisonywh@gmail.com>
2021-04-26 18:05:37 +0200

    Remove unused IO.inspect/1 and ran mix format

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Backoffice.MixProject do
       {:phoenix_html, "~> 2.11"},
       {:ecto_sql, "~> 3.1"},
       {:jason, "~> 1.0"},
-       {:mox, "~> 1.0", only: :test}
+      {:mox, "~> 1.0", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
