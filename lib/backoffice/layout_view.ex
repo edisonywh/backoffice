378fc4221df0c7ca4f0186bcea3743d40f26e81a
Kuria <012nkuria@gmail.com>
2021-05-03 20:13:55 +0300

    Remove render functions from layout_view.ex

--- lib/backoffice/layout_view.ex
+++ lib/backoffice/layout_view.ex
@@ -5,17 +5,6 @@ defmodule Backoffice.LayoutView do
     root: "lib/backoffice/templates",
     namespace: Backoffice
 
-  js_path = Path.join(__DIR__, "../../priv/static/js/app.js")
-  css_path = Path.join(__DIR__, "../../priv/static/css/app.css")
-
-  @external_resource js_path
-  @external_resource css_path
-
-  @app_js File.read!(js_path)
-  @app_css File.read!(css_path)
-
-  def render("app.js", _), do: @app_js
-  def render("app.css", _), do: @app_css
 
   def live_socket_path(conn) do
     [Enum.map(conn.script_name, &["/" | &1]) | conn.private.live_socket_path]
